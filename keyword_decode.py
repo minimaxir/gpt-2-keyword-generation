504b4c9ea19e5d49b678fd0eeb8c08a4f4b767c6
Max Woolf <max@minimaxir.com>
2019-05-26 20:30:02 -0700

    keyword_decode_file draft

--- keyword_decode.py
+++ keyword_decode.py
@@ -1,10 +1,4 @@
-import spacy
-import csv
 import re
-from tqdm import tqdm
-from itertools import chain
-from random import random, shuffle
-
 
 DELIMS = {
     'section': '~',
@@ -28,7 +22,7 @@ def keyword_decode(texts, input_sections=['keywords', 'titles'],
                    output_sections=['titles'],
                    pattern=None):
 
-    # get the index of the group we want to extract
+    # get the index of the group(s) we want to extract
     group_indices = [i for i, section in enumerate(input_sections)
                      if section in output_sections]
 
@@ -45,6 +39,25 @@ def keyword_decode(texts, input_sections=['keywords', 'titles'],
     return decoded_texts
 
 
-def keyword_decode_file():
+def keyword_decode_file(file_path, out_file='texts_decoded.txt',
+                        doc_delim='=' * 20 + '\n',
+                        input_sections=['keywords', 'titles'],
+                        output_sections=['titles'],
+                        start_token="<|startoftext|>",
+                        end_token="<|endoftext|>"):
 
+    section_pattern = build_pattern(input_sections)
+    doc_pattern = re.compile(
+        re.escape(start_token + '(.*)' + end_token), flags=re.MULTILINE)
 
+    with open(file_path, 'r', encoding='utf8', errors='ignore') as f:
+        # warning: loads entire file into memory!
+        docs = re.match(doc_pattern(f.read()))
+
+    decoded_docs = keyword_decode(docs, pattern=section_pattern,
+                                  input_sections=input_sections,
+                                  output_sections=output_sections)
+
+    with open(out_file, 'r', encoding='utf8', errors='ignore') as f:
+        for doc in decoded_docs:
+            f.write("{}\n{}".format(doc, doc_delim))
