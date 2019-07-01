a1b60324c185094ab737eb0677605a8b73f083e6
Max Woolf <max@minimaxir.com>
2019-06-30 17:07:45 -0700

    Fix regex issues

--- keyword_decode.py
+++ keyword_decode.py
@@ -9,26 +9,29 @@ DELIMS = {
 }
 
 
-def build_pattern(sections):
-    pattern_text = ''
+def build_pattern(sections, start_token, end_token):
+    pattern_text = re.escape(start_token) + '(?:.*)'
 
-    for section in section:
-        pattern_text = DELIMS['section'] + DELIMS[section] + '(.*)'
+    for section in sections:
+        pattern_text += '(?:{})'.format(
+            re.escape(DELIMS['section'] + DELIMS[section])) + '(.*)'
 
-    return re.compile(re.escape(pattern_text), flags=re.MULTILINE)
+    pattern_text += '(?:.*)' + re.escape(end_token)
+
+    return re.compile(pattern_text, flags=re.MULTILINE)
 
 
-def keyword_decode(texts, input_sections=['keywords', 'titles'],
-                   output_sections=['titles'],
-                   pattern=None):
+def keyword_decode(texts, input_sections=['keywords', 'title'],
+                   output_sections=['title'],
+                   start_token="<|startoftext|>",
+                   end_token="<|endoftext|>"):
 
     # get the index of the group(s) we want to extract
-    group_indices = [i for i, section in enumerate(input_sections)
+    group_indices = [i + 1 for i, section in enumerate(input_sections)
                      if section in output_sections]
 
     assert len(group_indices) > 0
-    if pattern is None:
-        pattern = build_pattern(input_sections)
+    pattern = build_pattern(input_sections, start_token, end_token)
     decoded_texts = []
     for text in texts:
         decoded_text = re.match(pattern, text)
@@ -46,7 +49,6 @@ def keyword_decode_file(file_path, out_file='texts_decoded.txt',
                         start_token="<|startoftext|>",
                         end_token="<|endoftext|>"):
 
-    section_pattern = build_pattern(input_sections)
     doc_pattern = re.compile(
         re.escape(start_token + '(.*)' + end_token), flags=re.MULTILINE)
 
@@ -54,10 +56,11 @@ def keyword_decode_file(file_path, out_file='texts_decoded.txt',
         # warning: loads entire file into memory!
         docs = re.match(doc_pattern(f.read()))
 
-    decoded_docs = keyword_decode(docs, pattern=section_pattern,
-                                  input_sections=input_sections,
-                                  output_sections=output_sections)
+    decoded_docs = keyword_decode(docs, input_sections=input_sections,
+                                  output_sections=output_sections,
+                                  start_token=start_token,
+                                  end_token=end_token)
 
-    with open(out_file, 'r', encoding='utf8', errors='ignore') as f:
+    with open(out_file, 'w', encoding='utf8', errors='ignore') as f:
         for doc in decoded_docs:
             f.write("{}\n{}".format(doc, doc_delim))
