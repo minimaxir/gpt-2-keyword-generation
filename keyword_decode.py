0029d7045f05bf7c632f7d661989d6d8327c6024
Max Woolf <max@minimaxir.com>
2021-01-18 11:37:23 -0800

    black format

--- keyword_decode.py
+++ keyword_decode.py
@@ -1,33 +1,28 @@
 import re
 
-DELIMS = {
-    'section': '~',
-    'category': '`',
-    'keywords': '^',
-    'title': '@',
-    'body': '}'
-}
+DELIMS = {"section": "~", "category": "`", "keywords": "^", "title": "@", "body": "}"}
 
 
 def build_pattern(sections, start_token, end_token):
     # sections may not be in the correct order: fix it
-    key_order = ['category', 'keywords', 'title', 'body']
+    key_order = ["category", "keywords", "title", "body"]
     sections = [section for section in key_order if section in sections]
 
-    pattern_text = re.escape(start_token) + '(?:.*)'
+    pattern_text = re.escape(start_token) + "(?:.*)"
 
     for section in sections:
-        pattern_text += '(?:{})'.format(
-            re.escape(DELIMS['section'] + DELIMS[section])) + '(.*)'
+        pattern_text += (
+            "(?:{})".format(re.escape(DELIMS["section"] + DELIMS[section])) + "(.*)"
+        )
 
-    pattern_text += '(?:.*)' + re.escape(end_token)
+    pattern_text += "(?:.*)" + re.escape(end_token)
 
     return re.compile(pattern_text, flags=re.MULTILINE)
 
 
-def decode_texts(texts, sections=['title'],
-                 start_token="<|startoftext|>",
-                 end_token="<|endoftext|>"):
+def decode_texts(
+    texts, sections=["title"], start_token="<|startoftext|>", end_token="<|endoftext|>"
+):
 
     # get the index of the group(s) we want to extract
     group_indices = [i + 1 for i, section in enumerate(sections)]
@@ -41,34 +36,38 @@ def decode_texts(texts, sections=['title'],
         decoded_text = re.match(pattern, text)
         if decoded_text is None:
             continue
-        decoded_text_attrs = tuple(decoded_text.group(i)
-                                   for i in group_indices)
+        decoded_text_attrs = tuple(decoded_text.group(i) for i in group_indices)
         if len(group_indices) == 1:
             decoded_text_attrs = decoded_text_attrs[0]
         decoded_texts.append(decoded_text_attrs)
     return decoded_texts
 
 
-def decode_file(file_path, out_file='texts_decoded.txt',
-                doc_delim='=' * 20 + '\n',
-                sections=['title'],
-                start_token="<|startoftext|>",
-                end_token="<|endoftext|>"):
+def decode_file(
+    file_path,
+    out_file="texts_decoded.txt",
+    doc_delim="=" * 20 + "\n",
+    sections=["title"],
+    start_token="<|startoftext|>",
+    end_token="<|endoftext|>",
+):
 
-    assert len(sections) == 1, "This function only supports output of a single section for now."
-    doc_pattern = re.compile(re.escape(start_token) +
-                             '(.*)' + re.escape(end_token), flags=re.MULTILINE)
+    assert (
+        len(sections) == 1
+    ), "This function only supports output of a single section for now."
+    doc_pattern = re.compile(
+        re.escape(start_token) + "(.*)" + re.escape(end_token), flags=re.MULTILINE
+    )
 
-    with open(file_path, 'r', encoding='utf8', errors='ignore') as f:
+    with open(file_path, "r", encoding="utf8", errors="ignore") as f:
         # warning: loads entire file into memory!
         docs = re.findall(doc_pattern, f.read())
 
     docs = [start_token + doc + end_token for doc in docs]
-    decoded_docs = decode_texts(docs,
-                                sections=sections,
-                                start_token=start_token,
-                                end_token=end_token)
+    decoded_docs = decode_texts(
+        docs, sections=sections, start_token=start_token, end_token=end_token
+    )
 
-    with open(out_file, 'w', encoding='utf8', errors='ignore') as f:
+    with open(out_file, "w", encoding="utf8", errors="ignore") as f:
         for doc in decoded_docs:
             f.write("{}\n{}".format(doc, doc_delim))
--- keyword_encode.py
+++ keyword_encode.py
@@ -3,43 +3,38 @@ import csv
 import re
 import ray
 import multiprocessing
-from functools import partial
 from tqdm import tqdm
 from itertools import chain
-from random import random, shuffle, randint
+from random import shuffle, randint
 
-DELIMS = {
-    'section': '~',
-    'category': '`',
-    'keywords': '^',
-    'title': '@',
-    'body': '}'
-}
+DELIMS = {"section": "~", "category": "`", "keywords": "^", "title": "@", "body": "}"}
 
-PRONOUN_LIST = ['I', 'Me', 'We', 'You', 'He', 'She',
-                'It', 'Him', 'Her', 'Them', 'They']
+PRONOUN_LIST = ["I", "Me", "We", "You", "He", "She", "It", "Him", "Her", "Them", "They"]
 
 PRONOUNS = set(PRONOUN_LIST + [x.lower() for x in PRONOUN_LIST])
 
 
-def encode_keywords(csv_path, model='en_core_web_sm',
-                    category_field=None,
-                    keywords_field=None,
-                    title_field=None,
-                    body_field=None,
-                    keyword_gen='title',
-                    keyword_sep=',',
-                    dropout=0.5,
-                    repeat=3,
-                    max_keywords=3,
-                    keyword_length_max=20,
-                    out_path='csv_encoded.txt',
-                    start_token="<|startoftext|>",
-                    end_token="<|endoftext|>"):
+def encode_keywords(
+    csv_path,
+    model="en_core_web_sm",
+    category_field=None,
+    keywords_field=None,
+    title_field=None,
+    body_field=None,
+    keyword_gen="title",
+    keyword_sep=",",
+    dropout=0.5,
+    repeat=3,
+    max_keywords=3,
+    keyword_length_max=20,
+    out_path="csv_encoded.txt",
+    start_token="<|startoftext|>",
+    end_token="<|endoftext|>",
+):
 
     data_list = []
 
-    with open(csv_path, 'r', encoding='utf8', errors='ignore') as f:
+    with open(csv_path, "r", encoding="utf8", errors="ignore") as f:
         reader = csv.DictReader(f)
         for row in reader:
             data_list.append(row)
@@ -48,29 +43,39 @@ def encode_keywords(csv_path, model='en_core_web_sm',
 
     # https://stackoverflow.com/a/434328
     def chunker(seq, size):
-        return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+        return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
     num_threads = multiprocessing.cpu_count() * 2  # colocate 2 processes per thread
     print("Starting up {} Workers".format(num_threads))
-    encoders = [Encoder.remote(model, category_field,
-                               keywords_field,
-                               title_field,
-                               body_field,
-                               keyword_gen,
-                               keyword_sep,
-                               repeat,
-                               max_keywords,
-                               keyword_length_max,
-                               start_token,
-                               end_token,
-                               DELIMS,
-                               PRONOUNS) for _ in range(num_threads)]
+    encoders = [
+        Encoder.remote(
+            model,
+            category_field,
+            keywords_field,
+            title_field,
+            body_field,
+            keyword_gen,
+            keyword_sep,
+            repeat,
+            max_keywords,
+            keyword_length_max,
+            start_token,
+            end_token,
+            DELIMS,
+            PRONOUNS,
+        )
+        for _ in range(num_threads)
+    ]
 
-    with open(out_path, 'w', encoding='utf8', errors='ignore') as w:
+    with open(out_path, "w", encoding="utf8", errors="ignore") as w:
         pbar = tqdm(total=len(data_list), smoothing=0)
         for chunk in chunker(data_list, num_threads):
-            results = ray.get([c.generate_encoded_text.remote(row)
-                               for c, row in list(zip(encoders, chunk))])
+            results = ray.get(
+                [
+                    c.generate_encoded_text.remote(row)
+                    for c, row in list(zip(encoders, chunk))
+                ]
+            )
 
             # unnest and randomize results
             results = list(chain.from_iterable(results))
@@ -84,21 +89,25 @@ def encode_keywords(csv_path, model='en_core_web_sm',
 
 @ray.remote(num_cpus=0.5)
 class Encoder(object):
-    def __init__(self, model, category_field,
-                 keywords_field,
-                 title_field,
-                 body_field,
-                 keyword_gen,
-                 keyword_sep,
-                 repeat,
-                 max_keywords,
-                 keyword_length_max,
-                 start_token,
-                 end_token,
-                 DELIMS,
-                 PRONOUNS):
+    def __init__(
+        self,
+        model,
+        category_field,
+        keywords_field,
+        title_field,
+        body_field,
+        keyword_gen,
+        keyword_sep,
+        repeat,
+        max_keywords,
+        keyword_length_max,
+        start_token,
+        end_token,
+        DELIMS,
+        PRONOUNS,
+    ):
         self.nlp = spacy.load(model)
-        self.pattern = re.compile('\W+')
+        self.pattern = re.compile("\W+")
 
         self.category_field = category_field
         self.keywords_field = keywords_field
@@ -116,8 +125,8 @@ class Encoder(object):
 
     def build_section(self, section, text):
         if text is None:
-            return ''
-        return self.DELIMS['section'] + self.DELIMS[section] + text
+            return ""
+        return self.DELIMS["section"] + self.DELIMS[section] + text
 
     def generate_encoded_text(self, row):
 
@@ -125,8 +134,11 @@ class Encoder(object):
         pattern = self.pattern
 
         # category should be normalized to account for user input
-        category = re.sub(
-            pattern, '-', row[self.category_field].lower().strip()) if self.category_field is not None else None
+        category = (
+            re.sub(pattern, "-", row[self.category_field].lower().strip())
+            if self.category_field is not None
+            else None
+        )
 
         title = row[self.title_field] if self.title_field is not None else None
         body = row[self.body_field] if self.body_field is not None else None
@@ -134,40 +146,51 @@ class Encoder(object):
         if self.keywords_field is None:
             # Generate the keywords using spacy
             # replace smart quotes first for better tokenization
-            text = re.sub(u'[\u2018\u2019]', "'",
-                          (re.sub(u'[\u201c\u201d]', '"', row[self.keyword_gen])))
+            text = re.sub(
+                u"[\u2018\u2019]",
+                "'",
+                (re.sub(u"[\u201c\u201d]", '"', row[self.keyword_gen])),
+            )
             doc = nlp(text)
-            keywords_pos = [chunk.text if chunk.pos_ == 'NOUN'
-                            else chunk.lemma_ if chunk.pos_ in ['VERB', 'ADJ', 'ADV']
-                            else 'I'
-                            for chunk in doc
-                            if not chunk.is_stop
-                            ]
-            keywords_ents = [re.sub(' ', '-', chunk.text)
-                             for chunk in doc.ents]
-            keywords_compounds = [re.sub(' ', '-', chunk.text)
-                                  for chunk in doc.noun_chunks
-                                  if len(chunk.text) < self.keyword_length_max]
+            keywords_pos = [
+                chunk.text
+                if chunk.pos_ == "NOUN"
+                else chunk.lemma_
+                if chunk.pos_ in ["VERB", "ADJ", "ADV"]
+                else "I"
+                for chunk in doc
+                if not chunk.is_stop
+            ]
+            keywords_ents = [re.sub(" ", "-", chunk.text) for chunk in doc.ents]
+            keywords_compounds = [
+                re.sub(" ", "-", chunk.text)
+                for chunk in doc.noun_chunks
+                if len(chunk.text) < self.keyword_length_max
+            ]
 
-            keywords = list(set(keywords_pos +
-                                keywords_ents +
-                                keywords_compounds) - self.PRONOUNS)  # dedupe
+            keywords = list(
+                set(keywords_pos + keywords_ents + keywords_compounds) - self.PRONOUNS
+            )  # dedupe
         else:
-            keywords = [keyword.strip()
-                        for keyword in row[self.keywords_field].split(self.keyword_sep)]
+            keywords = [
+                keyword.strip()
+                for keyword in row[self.keywords_field].split(self.keyword_sep)
+            ]
             keywords = list(set(keywords))
 
         encoded_texts = []
         for _ in range(self.repeat):
             new_keywords = keywords
             shuffle(new_keywords)
-            new_keywords = " ".join(
-                new_keywords[:randint(0, self.max_keywords)])
+            new_keywords = " ".join(new_keywords[: randint(0, self.max_keywords)])
 
-            encoded_texts.append(self.start_token +
-                                 self.build_section('category', category) +
-                                 self.build_section('keywords', new_keywords) +
-                                 self.build_section('title', title) +
-                                 self.build_section('body', body) +
-                                 self.end_token + "\n")
+            encoded_texts.append(
+                self.start_token
+                + self.build_section("category", category)
+                + self.build_section("keywords", new_keywords)
+                + self.build_section("title", title)
+                + self.build_section("body", body)
+                + self.end_token
+                + "\n"
+            )
         return encoded_texts
